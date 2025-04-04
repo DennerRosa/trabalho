4b7e3b29f91b0a54c9cbda7e986732b1824eb7c8
DENNER DOS SANTOS ROSA <denner.zolo50@gmail.com>
2025-04-04 19:49:47 -0300

    Add files via upload
    TRUNFO COM LOGICA

--- trunfo.c
+++ trunfo.c
@@ -1,38 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
-    
-    char carta = 'A';
-    char estado[50] = "Sao_Paulo";
-    char codigo = 'S';
-    int populacao = 45000000;
-    double pib = 1.180000000;
-    float area_km2 = 1520000.0;
-    int pontos_turisticos = 200;
- 
-    printf("carta: %c \n", carta);
-    printf("estado: %s \n", estado);
-    printf("codigo: %c \n", codigo);
-    printf("populacao: %d \n", populacao);
-    printf("pib: %f \n", pib);
-    printf("area_km2: %f \n", area_km2);
-    printf("pontos turisticos: %d \n", pontos_turisticos);
- 
-    char carta2 = 'B';
-    char estado2[50] = "Rio_de_Janeiro";
-    char codigo2 = 'R';
-    int populacao2 = 28000000;
-    double pib2 = 1.16000000;
-    float area_km22 = 1360000.0;
-    int pontos_turisticos2 = 1500;
- 
-    printf("carta: %c \n", carta2);
-    printf("estado: %s \n", estado2);
-    printf("codigo: %c \n", codigo2);
-    printf("populacao: %d \n", populacao2);
-    printf("pib: %f \n", pib2);
-    printf("area_km2: %f \n", area_km22);
-    printf("pontos turisticos: %d \n", pontos_turisticos2);
- 
+    int escolha_carta_jogador;
+    int atributo_escolhido;
+
+    // Dados da carta A
+    char estadoA[50] = "Sao Paulo";
+    char codigoA = 'S';
+    int populacaoA = 45000000;
+    double pibA = 1.180;
+    float areaA = 1520000.0;
+    int pontosA = 200;
+
+    // Dados da carta B
+    char estadoB[50] = "Rio de Janeiro";
+    char codigoB = 'R';
+    int populacaoB = 28000000;
+    double pibB = 1.160;
+    float areaB = 1360000.0;
+    int pontosB = 1500;
+
+    srand(time(0));
+
+    printf("*** SUPER TRUNFO ***\n\n");
+    printf("Escolha sua carta:\n");
+    printf("1 - Carta A (%s)\n", estadoA);
+    printf("2 - Carta B (%s)\n", estadoB);
+    printf("Digite 1 ou 2: ");
+    scanf("%d", &escolha_carta_jogador);
+
+    if (escolha_carta_jogador != 1 && escolha_carta_jogador != 2) {
+        printf("Escolha inválida!\n");
+        return 1;
+    }
+
+    // A carta do computador é a que o jogador não escolheu
+    int carta_computador = (escolha_carta_jogador == 1) ? 2 : 1;
+
+    printf("\nAgora escolha o atributo para competir:\n");
+    printf("1 - População\n");
+    printf("2 - PIB\n");
+    printf("3 - Área (km²)\n");
+    printf("4 - Pontos Turísticos\n");
+    printf("Digite o número do atributo: ");
+    scanf("%d", &atributo_escolhido);
+
+    printf("\n=== RESULTADO ===\n");
+
+    // Comparação dos atributos
+    switch (atributo_escolhido) {
+        case 1:
+            if (escolha_carta_jogador == 1)
+                printf("Sua população: %d\nPopulação do computador: %d\n", populacaoA, populacaoB);
+            else
+                printf("Sua população: %d\nPopulação do computador: %d\n", populacaoB, populacaoA);
+            break;
+        case 2:
+            if (escolha_carta_jogador == 1)
+                printf("Seu PIB: %.2lf\nPIB do computador: %.2lf\n", pibA, pibB);
+            else
+                printf("Seu PIB: %.2lf\nPIB do computador: %.2lf\n", pibB, pibA);
+            break;
+        case 3:
+            if (escolha_carta_jogador == 1)
+                printf("Sua área: %.2f km²\nÁrea do computador: %.2f km²\n", areaA, areaB);
+            else
+                printf("Sua área: %.2f km²\nÁrea do computador: %.2f km²\n", areaB, areaA);
+            break;
+        case 4:
+            if (escolha_carta_jogador == 1)
+                printf("Seus pontos turísticos: %d\nPontos do computador: %d\n", pontosA, pontosB);
+            else
+                printf("Seus pontos turísticos: %d\nPontos do computador: %d\n", pontosB, pontosA);
+            break;
+        default:
+            printf("Atributo inválido!\n");
+            return 1;
+    }
+
+    // Verifica quem ganhou
+    int jogador_venceu = 0, empate = 0;
+
+    switch (atributo_escolhido) {
+        case 1:
+            jogador_venceu = (escolha_carta_jogador == 1) ? populacaoA > populacaoB : populacaoB > populacaoA;
+            empate = populacaoA == populacaoB;
+            break;
+        case 2:
+            jogador_venceu = (escolha_carta_jogador == 1) ? pibA > pibB : pibB > pibA;
+            empate = pibA == pibB;
+            break;
+        case 3:
+            jogador_venceu = (escolha_carta_jogador == 1) ? areaA > areaB : areaB > areaA;
+            empate = areaA == areaB;
+            break;
+        case 4:
+            jogador_venceu = (escolha_carta_jogador == 1) ? pontosA > pontosB : pontosB > pontosA;
+            empate = pontosA == pontosB;
+            break;
+    }
+
+    if (empate)
+        printf("Empate!\n");
+    else if (jogador_venceu)
+        printf("Parabéns, você venceu!\n");
+    else
+        printf("Você perdeu! O computador venceu.\n");
+
+    return 0;
 }
